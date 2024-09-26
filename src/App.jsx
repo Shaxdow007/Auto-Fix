1c2725f8d0ae560ebe84d268d40215dfba62f82a
shadow007 <nourdinehoumame0@gmail.com>
2024-09-26 02:10:15 +0100

    install shadcn and tailwindCss

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <>
-      
+      <h1>hello world</h1>
+      <Button>click me</Button>
     </>
   )
 }
